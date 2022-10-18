6ec79b2d40ac30d361f0ec03205ea19fc5fde0c8
adinath412 <moreadinath412@gmail.com>
2022-10-18 20:58:33 +0530

    my project

---
+++ src/java/DeleteServlet.java
@@ -0,0 +1,22 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;  
+@WebServlet("/DeleteServlet")  
+public class DeleteServlet extends HttpServlet {  
+    
+
+	
+	private static final long serialVersionUID = 1L;
+
+        @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)   
+             throws ServletException, IOException {  
+        String sid=request.getParameter("id");  
+        int id=Integer.parseInt(sid);  
+        StudentDatabase.delete(id);  
+        response.sendRedirect("ViewServlet");  
+    }  
+}  
